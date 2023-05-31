44cb8a279edbd8bf1c434e4234d6e0e1b8d10e4d
demonassit <jminorg@ipn.mx>
2023-05-31 13:56:02 -0600

    problemas js

---
+++ 05Ejercicios/JS/interes.js
@@ -0,0 +1,27 @@
+//vamos a crear una funcion que se encarga de validar la entrada de datos
+
+function validarn(e){
+    var teclado = (document.all)?e.keyCode : e.which;
+
+    if(teclado == 8) return true;
+
+    var patron = /[0-9\d .]/;
+
+    var codigo = String.fromCharCode(teclado);
+
+    return patron.test(codigo);
+}
+
+function interes(){
+    var valor = document.formulario.cantidad.value;
+    var resul = parseInt(valor);
+    var interes = resul*0.02;
+    var total = interes + resul;
+
+    document.formulario.sueldoI.value = "$" + total;
+}
+
+function borrar(){
+    document.formulario.sueldoI.value = "";
+    document.formulario.cantidad.value = "";
+}
